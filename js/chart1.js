4c810129ea327abf5e3a8128b090be6d9a786f59
anahitahassan <67247100+anahitahassan@users.noreply.github.com>
2020-07-25 00:12:46 -0700

    Add files via upload

--- js/chart1.js
+++ js/chart1.js
@@ -4,14 +4,15 @@ window.onload = function () {
         zoomEnabled: true,
         theme: "light2",
         title:{
-            text: "Political Party vs Voter Turnout (by demographic group)"
+            text: "Republican Affliation vs Voter Turnout (by demographic group)"
         },
         axisX: {
-            title:"Political Lean",
-            suffix: "%",
-            minimum: 0,
-            maximum: 100,
-            gridThickness: 1
+                title:"Republican Political Lean",
+                suffix: "%",
+                minimum: 0,
+                maximum: 100,
+                gridThickness: 1,
+                margin: 10
         },
         axisY:{
             title: "Voter Turnout (in %)",
@@ -31,10 +32,10 @@ window.onload = function () {
             showInLegend: true,
             dataPoints: [ //https://www.pewresearch.org/fact-tank/2018/04/03/millennials-approach-baby-boomers-as-largest-generation-in-u-s-electorate/
                  //x = % republican, y = % voter turnout, z = size (millions) -->
-                { x: 32, y: 51, z:69, name:"18-35 (Millennial)" },
-                { x: 43, y: 63, z:57, name:"36-51 (Gen X)" },
-                { x: 46, y: 69, z:70, name:"52-70 (Boomer)" },
-                { x: 52, y: 70, z:28, name:"71+ (Silent/Greatest)"}
+                { x: 32, y: 51, z:30, name:"18-35 (Millennial)" },
+                { x: 43, y: 63, z:25, name:"36-51 (Gen X)" },
+                { x: 46, y: 69, z:31, name:"52-70 (Boomer)" },
+                { x: 52, y: 70, z:13, name:"71+ (Silent/Greatest)"}
             ]
         },
         {
@@ -43,15 +44,32 @@ window.onload = function () {
             toolTipContent: "<b>{name}</b><br/> % Republican: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
             showInLegend: true,
             dataPoints: [ 
-                 /*x = % republican, y = % voter turnout, z = size (millions) 
+                 /*x = % republican, y = % voter turnout, z = size (percentage of population) 
                  x: https://www.pewresearch.org/politics/2015/04/07/party-identification-trends-1992-2014/#education
                  y: https://www.census.gov/library/stories/2019/04/behind-2018-united-states-midterm-election-turnout.html
                  z: https://www.census.gov/library/stories/2019/02/number-of-people-with-masters-and-phd-degrees-double-since-2000.html 
                  */
-                { x: 37, y: 42.1, z:70.5, name:"High school diploma or less" },
-                { x: 42, y: 54.5, z:63.1, name:"Some college or associate's degree" },
-                { x: 40, y: 65.7, z:48.2, name:"Bachelor's degree"},
-                { x: 36, y: 74.0, z:28.7, name:"Advanced degree"}
+                { x: 37, y: 42.1, z:29.5, name:"High school diploma or less" },
+                { x: 42, y: 54.5, z:26.4, name:"Some college or associate's degree" },
+                { x: 40, y: 65.7, z:20.5, name:"Bachelor's degree"},
+                { x: 36, y: 74.0, z:12, name:"Advanced degree"}
+            ]
+        },
+        {
+            type: "bubble",
+            name: "Race",
+            toolTipContent: "<b>{name}</b><br/> % Republican: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
+            showInLegend: true,
+            dataPoints: [ 
+                 /*x = % republican, y = % voter turnout, z = size (percentage of population) 
+                 x: https://www.pewresearch.org/politics/2018/03/20/1-trends-in-party-affiliation-among-demographic-groups/
+                 y: https://www.census.gov/library/stories/2019/04/behind-2018-united-states-midterm-election-turnout.html 
+                 z: https://www.pewresearch.org/fact-tank/2019/05/01/historic-highs-in-2018-voter-turnout-extended-across-racial-and-ethnic-groups/
+                 */
+                { x: 51, y: 57.5, z:72.8, name:"White" },
+                { x: 8, y: 51.4, z:11.9, name:"Black" },
+                { x: 27, y: 40.2, z:3.5, name:"Asian"},
+                { x: 28, y: 40.4, z:9.6, name:"Hispanic/miscellaneous"}
             ]
         }]
     });
