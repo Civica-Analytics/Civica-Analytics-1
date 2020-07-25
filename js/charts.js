b7a11ffc50b7568ec704f80a93490a557bc87167
anahitahassan <67247100+anahitahassan@users.noreply.github.com>
2020-07-25 11:24:00 -0700

    Add files via upload

--- js/charts.js
+++ js/charts.js
@@ -73,7 +73,17 @@ window.onload = function () {
             ]
         }]
     });
-    chart.render(); 
+    chart.render();
+
+    function toggleDataSeries(e){
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        }
+        else{
+            e.dataSeries.visible = true;
+        }
+        chart.render();
+    }
 
     var chart = new CanvasJS.Chart("chartContainer2", {
         animationEnabled: true,
@@ -155,13 +165,7 @@ window.onload = function () {
     });
     chart.render();
 
-    function toggleDataSeries(e){
-        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-            e.dataSeries.visible = false;
-        }
-        else{
-            e.dataSeries.visible = true;
-        }
-        chart.render();
-    }
+    
+
+
 }
