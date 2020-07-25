5d87f8f36ca7c3f40a12a35a75fc66e35aa4dd5c
Swarnya Srivastava <49378967+Swarnya246@users.noreply.github.com>
2020-07-25 14:59:55 -0700

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -1,179 +1,191 @@
 window.onload = function () {
-    var chart = new CanvasJS.Chart("chartContainer1", {
-        animationEnabled: true,
-        zoomEnabled: true,
-        theme: "light2",
-        title:{
-            text: "Republican Affliation vs Voter Turnout (by demographic group)"
-        },
-        axisX: {
-                title:"Republican Political Lean",
-                suffix: "%",
-                minimum: 0,
-                maximum: 100,
-                gridThickness: 1,
-                margin: 10
-        },
-        axisY:{
-            title: "Voter Turnout (in %)",
-            suffix: "%",
-            minimum: 0,
-            maximum: 100
-        },
-        legend:{
-            cursor: "pointer",
-            fontSize: 16,
-            itemclick: toggleDataSeries
-        },
-        data: [{
-            type: "bubble",
-            name: "Age",
-            toolTipContent: "<b>{name}</b><br/> % Republican: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
-            showInLegend: true,
-            dataPoints: [ //https://www.pewresearch.org/fact-tank/2018/04/03/millennials-approach-baby-boomers-as-largest-generation-in-u-s-electorate/
-                 //x = % republican, y = % voter turnout, z = size (millions) -->
-                { x: 32, y: 51, z:30, name:"18-35 (Millennial)" },
-                { x: 43, y: 63, z:25, name:"36-51 (Gen X)" },
-                { x: 46, y: 69, z:31, name:"52-70 (Boomer)" },
-                { x: 52, y: 70, z:13, name:"71+ (Silent/Greatest)"}
-            ]
-        },
-        {
-            type: "bubble",
-            name: "Education Level",
-            toolTipContent: "<b>{name}</b><br/> % Republican: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
-            showInLegend: true,
-            dataPoints: [ 
-                 /*x = % republican, y = % voter turnout, z = size (percentage of population) 
-                 x: https://www.pewresearch.org/politics/2015/04/07/party-identification-trends-1992-2014/#education
-                 y: https://www.census.gov/library/stories/2019/04/behind-2018-united-states-midterm-election-turnout.html
-                 z: https://www.census.gov/library/stories/2019/02/number-of-people-with-masters-and-phd-degrees-double-since-2000.html 
-                 */
-                { x: 37, y: 42.1, z:29.5, name:"High school diploma or less" },
-                { x: 42, y: 54.5, z:26.4, name:"Some college or associate's degree" },
-                { x: 40, y: 65.7, z:20.5, name:"Bachelor's degree"},
-                { x: 36, y: 74.0, z:12, name:"Advanced degree"}
-            ]
-        },
-        {
-            type: "bubble",
-            name: "Race",
-            toolTipContent: "<b>{name}</b><br/> % Republican: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
-            showInLegend: true,
-            dataPoints: [ 
-                 /*x = % republican, y = % voter turnout, z = size (percentage of population) 
-                 x: https://www.pewresearch.org/politics/2018/03/20/1-trends-in-party-affiliation-among-demographic-groups/
-                 y: https://www.census.gov/library/stories/2019/04/behind-2018-united-states-midterm-election-turnout.html 
-                 z: https://www.pewresearch.org/fact-tank/2019/05/01/historic-highs-in-2018-voter-turnout-extended-across-racial-and-ethnic-groups/
-                 */
-                { x: 51, y: 57.5, z:72.8, name:"White" },
-                { x: 8, y: 51.4, z:11.9, name:"Black" },
-                { x: 27, y: 40.2, z:3.5, name:"Asian"},
-                { x: 28, y: 40.4, z:9.6, name:"Hispanic/miscellaneous"}
-            ]
-        }]
-    });
-    chart.render();
+var chart = new CanvasJS.Chart("chartContainer1", {
+	animationEnabled: true,
+	zoomEnabled: true,
+	theme: "light2",
+	toolTip: {
+      	  borderThickness: 10,
+      	  borderColor: "black",
+      	  cornerRadius: 15,
+          fontColor: "white",
+          backgroundColor: "black"
+    	},
+	axisX: {
+			title:"Republican Lean (%)",
+			suffix: "%",
+			minimum: 0,
+			maximum: 100,
+			gridThickness: 1,
+			margin: 10
+	},
+	axisY:{
+		title: "Voter Turnout (in %)",
+        suffix: "%",
+        minimum: 0,
+		maximum: 100
+    },
+    legend:{
+		cursor: "pointer",
+		fontSize: 16,
+		itemclick: toggleDataSeries
+    },
+	data: [{
+		type: "bubble",
+		name: "Age",
+		toolTipContent: "<b>{name}</b><br/> % Republican: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
+        showInLegend: true,
+		markerBorderColor: "black",
+        markerBorderThickness: 1,
+        dataPoints: [ //https://www.pewresearch.org/fact-tank/2018/04/03/millennials-approach-baby-boomers-as-largest-generation-in-u-s-electorate/
+             //x = % republican, y = % voter turnout, z = size (millions) -->
+            { x: 32, y: 51, z:30, name:"18-35 (Millennial)" },
+			{ x: 43, y: 63, z:25, name:"36-51 (Gen X)" },
+			{ x: 46, y: 69, z:31, name:"52-70 (Boomer)" },
+			{ x: 52, y: 70, z:13, name:"71+ (Silent/Greatest)"}
+		]
+    },
+    {
+        type: "bubble",
+		name: "Education Level",
+		toolTipContent: "<b>{name}</b><br/> % Republican: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
+        showInLegend: true,
+        dataPoints: [ 
+             /*x = % republican, y = % voter turnout, z = size (percentage of population) 
+			 x: https://www.pewresearch.org/politics/2015/04/07/party-identification-trends-1992-2014/#education
+			 y: https://www.census.gov/library/stories/2019/04/behind-2018-united-states-midterm-election-turnout.html
+			 z: https://www.census.gov/library/stories/2019/02/number-of-people-with-masters-and-phd-degrees-double-since-2000.html 
+			 */
+			{ x: 37, y: 42.1, z:29.5, name:"High school diploma or less" },
+			{ x: 42, y: 54.5, z:26.4, name:"Some college or associate's degree" },
+            { x: 40, y: 65.7, z:20.5, name:"Bachelor's degree"},
+            { x: 36, y: 74.0, z:12, name:"Advanced degree"}
+		]
+    },
+	{
+        type: "bubble",
+		name: "Race",
+		toolTipContent: "<b>{name}</b><br/> % Republican: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
+        showInLegend: true,
+        dataPoints: [ 
+             /*x = % republican, y = % voter turnout, z = size (percentage of population) 
+			 x: https://www.pewresearch.org/politics/2018/03/20/1-trends-in-party-affiliation-among-demographic-groups/
+			 y: https://www.census.gov/library/stories/2019/04/behind-2018-united-states-midterm-election-turnout.html 
+			 z: https://www.pewresearch.org/fact-tank/2019/05/01/historic-highs-in-2018-voter-turnout-extended-across-racial-and-ethnic-groups/
+			 */
+			{ x: 51, y: 57.5, z:72.8, name:"White" },
+			{ x: 8, y: 51.4, z:11.9, name:"Black" },
+            { x: 27, y: 40.2, z:3.5, name:"Asian"},
+            { x: 28, y: 40.4, z:9.6, name:"Hispanic/miscellaneous"}
+		]
+    }]
+});
+chart.render();
 
-    function toggleDataSeries(e){
-        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-            e.dataSeries.visible = false;
-        }
-        else{
-            e.dataSeries.visible = true;
-        }
-        chart.render();
-    }
+function toggleDataSeries(e){
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else{
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
 
     var chart = new CanvasJS.Chart("chartContainer2", {
-        animationEnabled: true,
-        zoomEnabled: true,
-        theme: "light2",
-        title:{
-            text: "Democratic Affliation vs Voter Turnout (by demographic group)"
-        },
-        axisX: {
-                title:"Democratic Lean (%)",
-                suffix: "%",
-                minimum: 0,
-                maximum: 100,
-                gridThickness: 1,
-                margin: 10
-        },
-        axisY:{
-            title: "Voter Turnout (in %)",
-            suffix: "%",
-            minimum: 0,
-            maximum: 100
-        },
-        legend:{
-            cursor: "pointer",
-            fontSize: 16,
-            itemclick: toggleDataSeries
-        },
-        data: [{
-            type: "bubble",
-            name: "Age",
-            toolTipContent: "<b>{name}</b><br/> % Democratic: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
-            showInLegend: true,
-            dataPoints: [ //
-                 /*x = % dem, y = % voter turnout, z = size (millions) 
-                 x: https://www.pewresearch.org/politics/2018/03/20/1-trends-in-party-affiliation-among-demographic-groups/
-                 z: https://www.pewresearch.org/fact-tank/2018/04/03/millennials-approach-baby-boomers-as-largest-generation-in-u-s-electorate/ 
-                 */
-                { x: 59, y: 51, z:30, name:"18-35 (Millennial)" },
-                { x: 48, y: 63, z:25, name:"36-51 (Gen X)" },
-                { x: 48, y: 69, z:31, name:"52-70 (Boomer)" },
-                { x: 43, y: 70, z:13, name:"71+ (Silent/Greatest)"}
-            ]
-        },
-        {
-            type: "bubble",
-            name: "Education Level",
-            toolTipContent: "<b>{name}</b><br/> % Democratic: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
-            showInLegend: true,
-            dataPoints: [ 
-                 /*x = % dem, y = % voter turnout, z = size (percentage of population) 
-                 x: https://www.pewresearch.org/politics/2015/04/07/party-identification-trends-1992-2014/#education
-                 y: https://www.census.gov/library/stories/2019/04/behind-2018-united-states-midterm-election-turnout.html
-                 z: https://www.census.gov/library/stories/2019/02/number-of-people-with-masters-and-phd-degrees-double-since-2000.html 
-                 */
-                { x: 47, y: 42.1, z:29.5, name:"High school diploma or less" },
-                { x: 47, y: 54.5, z:26.4, name:"Some college or associate's degree" },
-                { x: 52, y: 65.7, z:20.5, name:"Bachelor's degree"},
-                { x: 56, y: 74.0, z:12, name:"Advanced degree"}
-            ]
-        },
-        {
-            type: "bubble",
-            name: "Race",
-            toolTipContent: "<b>{name}</b><br/> % Democratic: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
-            showInLegend: true,
-            dataPoints: [ 
-                 /*x = % dem, y = % voter turnout, z = size (percentage of population) 
-                 x: https://www.pewresearch.org/politics/2018/03/20/1-trends-in-party-affiliation-among-demographic-groups/
-                 y: https://www.census.gov/library/stories/2019/04/behind-2018-united-states-midterm-election-turnout.html 
-                 z: https://www.pewresearch.org/fact-tank/2019/05/01/historic-highs-in-2018-voter-turnout-extended-across-racial-and-ethnic-groups/
-                 */
-                { x: 43, y: 57.5, z:72.8, name:"White" },
-                { x: 84, y: 51.4, z:11.9, name:"Black" },
-                { x: 65, y: 40.2, z:3.5, name:"Asian"},
-                { x: 63, y: 40.4, z:9.6, name:"Hispanic/miscellaneous"}
-            ]
-        }
-        ]
-    });
-    chart.render();
-
-    function toggleDataSeries(e){
-        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-            e.dataSeries.visible = false;
-        }
-        else{
-            e.dataSeries.visible = true;
-        }
-        chart.render();
+	animationEnabled: true,
+	zoomEnabled: true,
+	theme: "light2",
+	toolTip: {
+      	  borderThickness: 10,
+      	  borderColor: "black",
+      	  cornerRadius: 15,
+          fontColor: "white",
+          backgroundColor: "black"
+    	},
+	axisX: {
+			title:"Democratic Lean (%)",
+			suffix: "%",
+			minimum: 0,
+			maximum: 100,
+			gridThickness: 1,
+			margin: 10
+	},
+	axisY:{
+		title: "Voter Turnout (in %)",
+        suffix: "%",
+        minimum: 0,
+		maximum: 100
+    },
+    legend:{
+		cursor: "pointer",
+		fontSize: 16,
+		itemclick: toggleDataSeries
+    },
+	data: [{
+		type: "bubble",
+		name: "Age",
+		toolTipContent: "<b>{name}</b><br/> % Democratic: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
+        showInLegend: true,
+	markerBorderColor: "black",
+        markerBorderThickness: 1,
+        dataPoints: [ //
+             /*x = % dem, y = % voter turnout, z = size (millions) 
+             x: https://www.pewresearch.org/politics/2018/03/20/1-trends-in-party-affiliation-among-demographic-groups/
+             z: https://www.pewresearch.org/fact-tank/2018/04/03/millennials-approach-baby-boomers-as-largest-generation-in-u-s-electorate/ 
+             */
+            { x: 59, y: 51, z:30, name:"18-35 (Millennial)" },
+			{ x: 48, y: 63, z:25, name:"36-51 (Gen X)" },
+			{ x: 48, y: 69, z:31, name:"52-70 (Boomer)" },
+			{ x: 43, y: 70, z:13, name:"71+ (Silent/Greatest)"}
+		]
+    },
+    {
+        type: "bubble",
+		name: "Education Level",
+		toolTipContent: "<b>{name}</b><br/> % Democratic: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
+        showInLegend: true,
+        dataPoints: [ 
+             /*x = % dem, y = % voter turnout, z = size (percentage of population) 
+			 x: https://www.pewresearch.org/politics/2015/04/07/party-identification-trends-1992-2014/#education
+			 y: https://www.census.gov/library/stories/2019/04/behind-2018-united-states-midterm-election-turnout.html
+			 z: https://www.census.gov/library/stories/2019/02/number-of-people-with-masters-and-phd-degrees-double-since-2000.html 
+			 */
+			{ x: 47, y: 42.1, z:29.5, name:"High school diploma or less" },
+			{ x: 47, y: 54.5, z:26.4, name:"Some college or associate's degree" },
+            { x: 52, y: 65.7, z:20.5, name:"Bachelor's degree"},
+            { x: 56, y: 74.0, z:12, name:"Advanced degree"}
+		]
+    },
+	{
+        type: "bubble",
+		name: "Race",
+		toolTipContent: "<b>{name}</b><br/> % Democratic: {x}% <br/> Voter Turnout: {y}% <br/> Size (millions): {z}",
+        showInLegend: true,
+        dataPoints: [ 
+             /*x = % dem, y = % voter turnout, z = size (percentage of population) 
+			 x: https://www.pewresearch.org/politics/2018/03/20/1-trends-in-party-affiliation-among-demographic-groups/
+			 y: https://www.census.gov/library/stories/2019/04/behind-2018-united-states-midterm-election-turnout.html 
+			 z: https://www.pewresearch.org/fact-tank/2019/05/01/historic-highs-in-2018-voter-turnout-extended-across-racial-and-ethnic-groups/
+			 */
+			{ x: 43, y: 57.5, z:72.8, name:"White" },
+			{ x: 84, y: 51.4, z:11.9, name:"Black" },
+            { x: 65, y: 40.2, z:3.5, name:"Asian"},
+            { x: 63, y: 40.4, z:9.6, name:"Hispanic/miscellaneous"}
+		]
     }
+    ]
+});
+chart.render();
+
+function toggleDataSeries(e){
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else{
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
 
     var chart = new CanvasJS.Chart("chartContainer3", {
         animationEnabled: true,
