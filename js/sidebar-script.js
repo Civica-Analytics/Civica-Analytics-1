2a76f23f1155fdf8a541d9b573aa222c77449cd2
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-07-25 03:23:45 -0700

    change how much the sidebar opens

--- js/sidebar-script.js
+++ js/sidebar-script.js
@@ -1,7 +1,7 @@
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "25vw"; //opens to 25% of screen, able to see words clearer
+    document.getElementById("main").style.marginLeft = "0px"; //leaving the bottom unchanged
 }
   
 function closeNav() {
